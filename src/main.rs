9d8a7595e7183e505797bfdeb2001ecf63879de9
Ashton Sawyer <ashtonesawyer@gmail.com>
2025-11-04 14:47:29 -0800

    outlining ticket info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+/// How difficult a ticket is to complete
+enum Difficulty {
+    Easy,
+    Med,
+    Hard,
+}
+
+/// Categories that tickets can full under
+enum Category {
+    Network,
+    Windows,
+    Linux,
+    Web,
+    Misc,
+}
+
+struct Currency {
+    cash: u64,
+    xp: u64,
+}
+
+/// Ticket object 
+struct Ticket {
+    /// How hard the ticket is to complete
+    difficulty: Difficulty,
+    /// What kind of ticket it is
+    category: Category,
+    /// How many times the ticket has already been clicked
+    clicked: u16,
+    /// Name of the ticket
+    name: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
