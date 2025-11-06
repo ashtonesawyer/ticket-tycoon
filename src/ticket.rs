192c54533ffff74c26543f32e8e6d1f23aec69b8
Ashton Sawyer <ashtonesawyer@gmail.com>
2025-11-05 19:43:10 -0800

    start ticket impl

--- src/ticket.rs
+++ src/ticket.rs
@@ -1,4 +1,21 @@
+#[test]
+fn easy_network() {
+    let ticket = Ticket::new(Difficulty::Easy, Category::Network, "name1");
+    assert_eq!(ticket.difficulty(), &Difficulty::Easy);
+    assert_eq!(ticket.category(), &Category::Network);
+    assert_eq!(ticket.clicked(), 0);
+}
+
+#[test]
+fn hard_web() {
+    let ticket = Ticket::new(Difficulty::Hard, Category::Web, "name2");
+    assert_eq!(ticket.difficulty(), &Difficulty::Hard);
+    assert_eq!(ticket.category(), &Category::Web);
+    assert_eq!(ticket.clicked(), 0);
+}
+
 /// How difficult a ticket is to complete
+#[derive(Debug, PartialEq)]
 pub enum Difficulty {
     Easy,
     Med,
@@ -6,6 +23,7 @@ pub enum Difficulty {
 }
 
 /// Categories that tickets can full under
+#[derive(Debug, PartialEq)]
 pub enum Category {
     Network,
     Windows,
@@ -14,12 +32,15 @@ pub enum Category {
     Misc,
 }
 
+#[derive(Debug)]
 pub struct Currency {
     cash: u64,
     xp: u64,
 }
 
+
 /// Ticket object 
+#[derive(Debug)]
 pub struct Ticket {
     /// How hard the ticket is to complete
     difficulty: Difficulty,
@@ -29,4 +50,27 @@ pub struct Ticket {
     clicked: u16,
     /// Name of the ticket
     name: String,
+}
+
+impl Ticket {
+    pub fn new(difficulty: Difficulty, category: Category, name: &str) -> Self {
+        Self {
+            difficulty,
+            category,
+            clicked: 0,
+            name: name.to_string(),
+        }
+    }
+
+    pub fn difficulty(&self) -> &Difficulty {
+        &self.difficulty
+    }
+
+    pub fn category(&self) -> &Category {
+        &self.category
+    }
+
+    pub fn clicked(self) -> u16 {
+        self.clicked
+    }
 }
