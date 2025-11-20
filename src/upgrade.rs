eca340aa96cd017f479d980f880946fdc4958244
Ashton Sawyer <ashtonesawyer@gmail.com>
2025-11-19 19:44:56 -0800

    fmt

--- src/upgrade.rs
+++ src/upgrade.rs
@@ -5,12 +5,34 @@ use serde_json::Result;
 
 #[test]
 fn to_json() {
-    let upgrade1 = Upgrade::new("test-upgrade", "This is just a test", Currency::new(), Vec::new(), Effects { inc_multiplier: 0.2});
-    let upgrade2 = Upgrade::new("test-upgrade", "This is just a test", Currency::new(), vec!["some-upgrade".to_string(), "some-other-upgrade".to_string()], Effects { inc_multiplier: 0.2});
+    let upgrade1 = Upgrade::new(
+        "test-upgrade",
+        "This is just a test",
+        Currency::new(),
+        Vec::new(),
+        Effects {
+            inc_multiplier: 0.2,
+        },
+    );
+    let upgrade2 = Upgrade::new(
+        "test-upgrade",
+        "This is just a test",
+        Currency::new(),
+        vec!["some-upgrade".to_string(), "some-other-upgrade".to_string()],
+        Effects {
+            inc_multiplier: 0.2,
+        },
+    );
     let json1 = serde_json::to_string(&upgrade1).unwrap();
     let json2 = serde_json::to_string(&upgrade2).unwrap();
-    assert_eq!(json1, "{\"id\":\"test-upgrade\",\"desc\":\"This is just a test\",\"cost\":{\"cash\":0,\"xp\":0},\"requires\":[],\"effects\":{\"inc_multiplier\":0.2}}");
-    assert_eq!(json2, "{\"id\":\"test-upgrade\",\"desc\":\"This is just a test\",\"cost\":{\"cash\":0,\"xp\":0},\"requires\":[\"some-upgrade\",\"some-other-upgrade\"],\"effects\":{\"inc_multiplier\":0.2}}");
+    assert_eq!(
+        json1,
+        "{\"id\":\"test-upgrade\",\"desc\":\"This is just a test\",\"cost\":{\"cash\":0,\"xp\":0},\"requires\":[],\"effects\":{\"inc_multiplier\":0.2}}"
+    );
+    assert_eq!(
+        json2,
+        "{\"id\":\"test-upgrade\",\"desc\":\"This is just a test\",\"cost\":{\"cash\":0,\"xp\":0},\"requires\":[\"some-upgrade\",\"some-other-upgrade\"],\"effects\":{\"inc_multiplier\":0.2}}"
+    );
 }
 
 #[test]
@@ -19,8 +41,24 @@ fn from_json() {
     let json2 = "{\"id\":\"test-upgrade\",\"desc\":\"This is just a test\",\"cost\":{\"cash\":0,\"xp\":0},\"requires\":[\"some-upgrade\",\"some-other-upgrade\"],\"effects\":{\"inc_multiplier\":0.2}}";
     let parsed1: Upgrade = serde_json::from_str(&json1).unwrap();
     let parsed2: Upgrade = serde_json::from_str(&json2).unwrap();
-    let upgrade1 = Upgrade::new("test-upgrade", "This is just a test", Currency::new(), Vec::new(), Effects { inc_multiplier: 0.2});
-    let upgrade2 = Upgrade::new("test-upgrade", "This is just a test", Currency::new(), vec!["some-upgrade".to_string(), "some-other-upgrade".to_string()], Effects { inc_multiplier: 0.2});
+    let upgrade1 = Upgrade::new(
+        "test-upgrade",
+        "This is just a test",
+        Currency::new(),
+        Vec::new(),
+        Effects {
+            inc_multiplier: 0.2,
+        },
+    );
+    let upgrade2 = Upgrade::new(
+        "test-upgrade",
+        "This is just a test",
+        Currency::new(),
+        vec!["some-upgrade".to_string(), "some-other-upgrade".to_string()],
+        Effects {
+            inc_multiplier: 0.2,
+        },
+    );
     assert_eq!(parsed1, upgrade1);
     assert_eq!(parsed2, upgrade2);
 }
@@ -42,13 +80,19 @@ pub struct Upgrade {
     /// Cost in cash + xp
     pub cost: Currency,
     /// What upgrades are needed before it becomes available
-    pub requires: Vec<String>, 
-    /// What the upgrade actually does 
+    pub requires: Vec<String>,
+    /// What the upgrade actually does
     pub effects: Effects,
 }
 
 impl Upgrade {
-    pub fn new(id: &str, desc: &str, cost: Currency, requires: Vec<String>, effects: Effects) -> Self {
+    pub fn new(
+        id: &str,
+        desc: &str,
+        cost: Currency,
+        requires: Vec<String>,
+        effects: Effects,
+    ) -> Self {
         Self {
             id: id.to_string(),
             desc: desc.to_string(),
