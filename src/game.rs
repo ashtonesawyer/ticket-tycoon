8bd32763a2794c7ecaab9d0c527900321eaa558c
Ashton Sawyer <ashtonesawyer@gmail.com>
2025-11-24 22:26:09 -0800

    fmt

--- src/game.rs
+++ src/game.rs
@@ -120,35 +120,47 @@ fn click_multiplier_75() {
 fn upgrade_happy() {
     let mut game = GameState::new();
     game.wallet.add_xp(30);
-    assert!(game.buy_upgrade(&"wrist_stretch_reminder".to_string()).is_ok());
+    assert!(
+        game.buy_upgrade(&"wrist_stretch_reminder".to_string())
+            .is_ok()
+    );
     assert_eq!(game.wallet.xp(), 0);
 }
 
 #[test]
 fn upgrade_unavail() {
     let mut game = GameState::new();
-    assert!(game.buy_upgrade(&"fake_upgrade_id".to_string()).is_err_and(|x| match x {
-        BuyError::UpgradeUnavailable => true,
-        _ => false,
-    }));
+    assert!(
+        game.buy_upgrade(&"fake_upgrade_id".to_string())
+            .is_err_and(|x| match x {
+                BuyError::UpgradeUnavailable => true,
+                _ => false,
+            })
+    );
 }
 
 #[test]
 fn upgrade_insuff_cash() {
     let mut game = GameState::new();
-    assert!(game.buy_upgrade(&"slightly_less_terrible_mouse".to_string()).is_err_and(|x| match x {
-        BuyError::Wallet(WalletError::InsufficientCash) => true,
-        _ => false,
-    }));
+    assert!(
+        game.buy_upgrade(&"slightly_less_terrible_mouse".to_string())
+            .is_err_and(|x| match x {
+                BuyError::Wallet(WalletError::InsufficientCash) => true,
+                _ => false,
+            })
+    );
 }
 
 #[test]
 fn upgrade_insuff_xp() {
     let mut game = GameState::new();
-    assert!(game.buy_upgrade(&"wrist_stretch_reminder".to_string()).is_err_and(|x| match x {
-        BuyError::Wallet(WalletError::InsufficientXP) => true,
-        _ => false,
-    }));
+    assert!(
+        game.buy_upgrade(&"wrist_stretch_reminder".to_string())
+            .is_err_and(|x| match x {
+                BuyError::Wallet(WalletError::InsufficientXP) => true,
+                _ => false,
+            })
+    );
 }
 
 pub enum BuyError {
