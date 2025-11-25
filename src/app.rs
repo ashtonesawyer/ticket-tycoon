b14555dc3469fb3d07011222d9014372e2a405ad
Ashton Sawyer <ashtonesawyer@gmail.com>
2025-11-24 22:25:45 -0800

    basic styling

--- src/app.rs
+++ src/app.rs
@@ -15,28 +15,44 @@ pub fn app() -> Element {
 
     rsx! {
         Header { cash: state.read().wallet().cash(), xp: state.read().wallet().xp() }
-        Error { err: error.read() }
-        Queue { queue: state.read().working().to_vec(), on_click: move |i| {
-            error.set(String::new());
-            state.write().click_ticket(i)
+        div {
+            style: "display: flex; flex-direction: row; justify-content: space-around; padding: 15px 5px; min-height: 385px",
+
+            Queue {
+                queue: state.read().working().to_vec(),
+                on_click: move |i| {
+                    error.set(String::new());
+                    state.write().click_ticket(i)
+                }
+            }
+            Upgrades {
+                upgrades: state.read().avail_upgrades(),
+                on_click: move |id| {
+                    error.set(String::new());
+                    match state.write().buy_upgrade(&id) {
+                        Ok(()) => {return},
+                        Err(BuyError::UpgradeUnavailable) => panic!("Should check for availability before showing to user"),
+                        Err(BuyError::Wallet(WalletError::InsufficientCash)) => error.set("Not enough cash to buy this upgrade".to_string()),
+                        Err(BuyError::Wallet(WalletError::InsufficientXP)) => error.set("Not enough XP to buy this upgrade".to_string()),
+                    }
+                }
             }
         }
-        Upgrades { upgrades: state.read().avail_upgrades(), on_click: move |id| {
-            error.set(String::new());
-            match state.write().buy_upgrade(&id) {
-                Ok(()) => {return},
-                Err(BuyError::UpgradeUnavailable) => panic!("Should check for availability before showing to user"),
-                Err(BuyError::Wallet(WalletError::InsufficientCash)) => error.set("Not enough cash to buy this upgrade".to_string()),
-                Err(BuyError::Wallet(WalletError::InsufficientXP)) => error.set("Not enough XP to buy this upgrade".to_string()),
-            }
-        }}
+        div {
+            style: "border-bottom: 1px solid black; padding-top: 30px",
+        }
+        Error { err: error.read() }
     }
 }
 
 #[component]
 fn Header(cash: u64, xp: u64) -> Element {
     rsx! {
-        span { "Cash: {cash} | XP: {xp}" }
+        div {
+            style: "padding: 0 30px 15px 0; text-align: right; border-bottom: 1px solid black;",
+
+            span { "[ ${cash} ]  [ {xp} XP ]" }
+        }
     }
 }
 
@@ -44,6 +60,8 @@ fn Header(cash: u64, xp: u64) -> Element {
 fn Error(err: String) -> Element {
     rsx! {
         div {
+            style: "padding: 30px; color: red; text-align: center; font-weight: bold",
+
             "{err}"
         }
     }
@@ -53,7 +71,9 @@ fn Error(err: String) -> Element {
 fn Queue(queue: Vec<Ticket>, on_click: EventHandler<usize>) -> Element {
     rsx! {
         div {
-            style: "display: flex; flex-direction: column; gap: 12px;",
+            style: "width: 375px; display: flex; flex-direction: column; gap: 10px;",
+
+            h3 { "Ticket Queue" }
 
             for (i, ticket) in queue.iter().enumerate() {
                 {
@@ -61,12 +81,23 @@ fn Queue(queue: Vec<Ticket>, on_click: EventHandler<usize>) -> Element {
 
                     rsx! {
                         div {
+                            style: "border: 1px solid black; padding: 5px; display: flex; flex-direction: column; gap: 20px;",
                             key: "{i}",
-                            p { "{ticket.name()}" }
-                            p { {format!("{:?} | {:?} | {pct:.0}%", ticket.category(), ticket.difficulty()) }}
-                            button {
-                                onclick: move |_| on_click.call(i),
-                                "Work"
+
+                            div {
+                                style: "display: flex; justify-content: space-between;",
+
+                                span { "{ticket.name()}" }
+                                span { {format!("{:?} - {:?}", ticket.category(), ticket.difficulty()) }}
+                            }
+                            div {
+                                style: "display: flex; justify-content: space-between;",
+
+                                ProgBar { pct }
+                                button {
+                                    onclick: move |_| on_click.call(i),
+                                    "Work"
+                                }
                             }
                         }
                     }
@@ -77,20 +108,61 @@ fn Queue(queue: Vec<Ticket>, on_click: EventHandler<usize>) -> Element {
 }
 
 #[component]
+fn ProgBar(pct: f32) -> Element {
+    let width = 20;
+    let filled = ((pct / 100.0) * width as f32).round() as usize;
+    let empty = width - filled;
+
+    let bar = format!(
+        "[{}{}] {}%",
+        "#".repeat(filled),
+        ".".repeat(empty),
+        pct as usize,
+    );
+
+    rsx! {
+        div {
+            style: "font-family: monospace;",
+            "{bar}"
+        }
+    }
+}
+
+#[component]
 fn Upgrades(upgrades: Vec<Upgrade>, on_click: EventHandler<String>) -> Element {
     rsx! {
         div {
+            style: "width: 310px; display: flex; flex-direction: column; gap: 10px",
+
+            h3 { "Upgrades" }
             for (i, upgrade) in upgrades.iter().enumerate() {
                 {
                     let id = upgrade.id.clone();
                     rsx! {
                         div {
+                            style: "border: 1px solid black; padding: 5px;",
                             key: "{i}",
-                            h3 { "{upgrade.name}" }
+
+                            h4 { "{upgrade.name}" }
                             p  { "{upgrade.desc}" }
-                            button {
-                                onclick: move |_| on_click.call(id.clone()),
-                                "Buy"
+                            div {
+                                style: "display: flex; flex-direction: row; justify-content: space-between",
+
+                                {
+                                    let mut cost = match (upgrade.cost.cash(), upgrade.cost.xp()) {
+                                        (0, 0) => "".to_string(),
+                                        (x, 0) => format!("${}", x),
+                                        (0, x) => format!("{} XP", x),
+                                        (x, y) => format!("${} and {} XP", x, y),
+                                    };
+                                    rsx! {
+                                        span { "{cost}" }
+                                        button {
+                                            onclick: move |_| on_click.call(id.clone()),
+                                            "Buy"
+                                        }
+                                    }
+                                }
                             }
                         }
                     }
